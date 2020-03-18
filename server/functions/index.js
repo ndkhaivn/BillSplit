5c205afffcdf5611695648f93ecfdaca18c4a83c
Khai Nguyen <dngu0028@student.monash.edu>
2020-03-18 22:18:22 +0700

    Working APIs for GET & POST /tenants
    GET /tenants for getting all tenants
    POST /tenants for adding new tenants

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,8 +1,47 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+admin.initializeApp();
+
+const express = require('express');
+const app = express();
+
+app.get('/tenants', (request, response) => {
+    admin
+        .firestore()
+        .collection('tenants')
+        .get()
+        .then((data) => {
+            let tenants = [];
+            data.forEach((doc) => {
+                tenants.push(doc.data());
+            });
+            return response.json(tenants);
+        })
+        .catch((err) => {
+            response.status(500).json({ error: err.code });
+            console.log(err.code);
+        });
 });
+
+app.post('/tenants', (request, response) => {
+
+    const newTenant = {
+        name: request.body.name,
+        stays: request.body.stays
+    };
+
+    admin
+        .firestore()
+        .collection('tenants')
+        .add(newTenant)
+        .then((doc) => {
+            return response.json({ message: `document ${doc.id} created successfully` });
+        })
+        .catch((err) => {
+            response.status(500).json({ error: `could not add tenant` });
+            console.log(err);
+        })
+});
+
+exports.api = functions.region('asia-east2').https.onRequest(app);
