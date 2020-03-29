328dc45f5fc67c6675bb6b2750f8224c602ff2eb
ndkhaivn <ndkhaivn@gmail.com>
2020-03-30 02:24:32 +0700

    Allow Access-Controll-Allow-Origin: * header

--- server/functions/index.js
+++ server/functions/index.js
@@ -7,6 +7,12 @@ const functions = require('firebase-functions');
 const express = require('express');
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/tenants', tenantController.getAllTenants);
 app.get('/tenant/:tenantId', tenantController.getTenant);
 app.post('/tenants', tenantController.validate('addTenant'), tenantController.addTenant);
